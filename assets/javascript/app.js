24290331f3cb14ad754d0117a79b89c3dc4e17a8
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-06-20 20:22:35 -0500

    first draft

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+
+let timer;
+let audio = new Audio("assets/audio/mrrogers.mp3");
+let number = 90;
+
+window.onload = function () {
+    $(".quiz-content").hide();
+    $("#start").on("click", run);
+}
+
+function run() {
+    timer = setInterval(decrement, 1000);
+    audio.play();
+    $(".quiz-content").show();
+    $("#start").hide();
+}
+
+function decrement() {
+    number--;
+    $("#timer").html(`Time Left: ${number}`);
+
+
+    //  Once number hits zero...
+    if (number === 0) {
+        $("#timer").hide();
+        $(".quiz-content").hide();
+    }
+}
