5338b4099ecd4cf860a70f994eac4fada3bcf6ba
LefseTime <36971616+LefseTime@users.noreply.github.com>
2018-06-21 20:00:15 -0500

    first draft of finished game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,78 @@
-
-let timer;
-let audio = new Audio("assets/audio/mrrogers.mp3");
-let number = 90;
-let audio2 = new Audio("assets/audio/itsYouILike.mp3");
-
 window.onload = function () {
+
+    
+    let number = 90;
+    let correctAnswer = 0;
+    let audio = new Audio("assets/audio/mrrogers.mp3");
+    let audio2 = new Audio("assets/audio/itsYouILike.mp3");
+
     $(".quiz-content").hide();
+    $(".results-section").hide();
     $("#start").on("click", run);
-}
-
-function run() {
-    timer = setInterval(decrement, 1000);
-    audio.play();
-    $(".quiz-content").show();
-    $("#start").hide();
-}
-
-function decrement() {
-    number--;
-    $("#timer").html(`Time Left: ${number}`);
+    $("#submit").on("click", results);
+    $("#reset").on("click", reset);
 
 
-    //  Once number hits zero...
-    if (number === 0) {
+    function reset(){
+        window.location.reload();
+        // number = 90;
+        // correctAnswer = 0;
+        // $("#timer").show();
+        // $("#start").show();
+        // $(".quiz-content").hide();
+        // $(".results-section").hide();
+        // audio2.pause();
+    }
+
+    function run() {
+        setInterval(decrement, 1000);
+        audio.play();
+        $(".quiz-content").show();
+        $("#start").hide();
+    }
+
+    function decrement() {
+        number--;
+        $("#timer").html(`Time Left: ${number}`);
+
+        //  Once number hits zero
+        if (number === 0) {
+            results();
+        };
+       //click handler for finish button, grab input values
+    }
+
+
+
+    function results() {
         $("#timer").hide();
         $(".quiz-content").hide();
+        $(".results-section").show();
+        window.scrollTo(0, 0);
+        audio.pause();
         audio2.play();
+
+        let selectedAnswer1 = $('input[name=q1]:checked').val();
+        let selectedAnswer2 = $('input[name=q2]:checked').val();
+        let selectedAnswer3 = $('input[name=q3]:checked').val();
+        let selectedAnswer4 = $('input[name=q4]:checked').val();
+        let selectedAnswer5 = $('input[name=q5]:checked').val();
+
+        if (selectedAnswer1 === "option2") {
+            correctAnswer++;
+        }
+        if (selectedAnswer2 === "option2") {
+            correctAnswer++;
+        }
+        if (selectedAnswer3 === "option1") {
+            correctAnswer++;
+        }
+        if (selectedAnswer4 === "option1") {
+            correctAnswer++;
+        }
+        if (selectedAnswer5 === "option4") {
+            correctAnswer++;
+        }
+        $("#writeResults").text(`${correctAnswer} out of 5`);
     }
 }
